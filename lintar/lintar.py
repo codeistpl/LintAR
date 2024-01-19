31cb1b2df7d338f7aa73da2aedb919b330a7bc93
Michal Sopniewski <codeist@codeist.pl>
2024-01-19 17:54:17 +0100

    upgraded logging config

--- lintar/lintar.py
+++ lintar/lintar.py
@@ -4,6 +4,8 @@ import logging
 import colorlog
 from arxml.parser import ArxmlParser
 from validators.schema_based_validator import validate_arxml_with_schema
+import os
+import os
 
 
 def configure_logger(log_file=None, log_level=logging.INFO):
@@ -19,42 +21,64 @@ def configure_logger(log_file=None, log_level=logging.INFO):
     if logging.getLogger().hasHandlers():
         return
 
-    # Create a color formatter
-    formatter = colorlog.ColoredFormatter(
-        "%(asctime)s %(log_color)s%(levelname)-8s"
-        + "%(reset)s %(filename)s::%(lineno)d - %(message)s",
-        log_colors={
-            "DEBUG": "reset",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "bold_red",
-        },
-        datefmt="%d-%M-%Y %H:%M:%S",
-    )
-
-    # Create a console handler and set the formatter
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-
-    # Add the console handler to the logger
     logger = logging.getLogger()
-    logger.addHandler(console_handler)
-    logger.setLevel(log_level)
 
     # Check if log_file is provided
     if log_file:
         # Create a file handler and set the formatter
         file_handler = logging.FileHandler(log_file)
+        fmt = (
+            "%(asctime)s| %(levelname)-8s| %(filename)s:%(lineno)d | %(message)s"
+            if log_level == logging.DEBUG
+            else "%(asctime)s| %(levelname)-8s| %(message)s"
+        )
+        formatter = logging.Formatter(
+            fmt=fmt,
+        )
         file_handler.setFormatter(formatter)
 
         # Add the file handler to the logger
         logger.addHandler(file_handler)
 
+    else:  # log to console
+        fmt = (
+            "%(log_color)s%(levelname)-8s%(reset)s| %(filename)s:%(lineno)d | %(message)s"
+            if log_level == logging.DEBUG
+            else "%(log_color)s%(levelname)-8s%(reset)s| %(message)s"
+        )
+
+        colored_formatter = colorlog.ColoredFormatter(
+            fmt,
+            log_colors={
+                "DEBUG": "reset",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
+            datefmt="%d-%M-%Y %H:%M:%S,uuu",
+        )
+        # Create a console handler and set the formatter
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(colored_formatter)
+
+        # Add the console handler to the logger
+
+        logger.addHandler(console_handler)
+    logger.setLevel(log_level)
+
 
 def main():
     # Parse command-line arguments
     version = "0.1.0"
+    print(os.path.abspath(__file__))
+    print(os.path.abspath(os.path.curdir))
+    print(
+        os.path.relpath(
+            os.path.abspath(__file__), os.path.abspath(os.path.curdir)
+        )
+        + ":80"
+    )
     parser = argparse.ArgumentParser(
         description="LintAR the ARXML linting tool"
     )
@@ -64,13 +88,15 @@ def main():
     parser.add_argument(
         "--log-level", default="INFO", help="Set the log level (default: INFO)"
     )
+    parser.add_argument("--log-file", help="Path to the log file")
+
     parser.add_argument(
         "--version", action="version", version=f"%(prog)s {version}"
     )
 
     args = parser.parse_args()
 
-    configure_logger(log_level=args.log_level)
+    configure_logger(log_level=args.log_level, log_file=args.log_file)
 
     validate_arxml_with_schema(args.file)
 
